fb5f2c25276f7ab053031d1f11be69a483d47aba
PhantomBridger <210878029+PhantomBridger@users.noreply.github.com>
2025-09-05 18:13:23 +0200

    Import fabric template

---
+++ src/main/java/dev/phantombridger/enhancedoverlay/mixin/ScoreboardMixin.java
@@ -0,0 +1,12 @@
+package dev.phantombridger.enhancedoverlay.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class ScoreboardMixin {
+
+}
