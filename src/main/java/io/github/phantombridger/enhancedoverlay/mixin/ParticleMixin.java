a956218f4912d2c00ec20379c32d39089a5b7013
PhantomBridger <210878029+PhantomBridger@users.noreply.github.com>
2026-08-09 21:04:09 +0200

    Fixed bug where the hideFirstPersonEffectParticles option broke the timer of potion effects in the inventory

--- src/main/java/io/github/phantombridger/enhancedoverlay/mixin/ParticleMixin.java
+++ src/main/java/io/github/phantombridger/enhancedoverlay/mixin/ParticleMixin.java
@@ -2,22 +2,24 @@ package io.github.phantombridger.enhancedoverlay.mixin;
 
 import io.github.phantombridger.enhancedoverlay.config.ConfigScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.List;
 
 @Mixin(LivingEntity.class)
 public class ParticleMixin {
-    @Inject(method = "tickEffects", at = @At("HEAD"), cancellable = true)
-    private void removeFirstPersonEffectParticles(CallbackInfo ci) {
-        LivingEntity entity = (LivingEntity) (Object) this;
+    @ModifyVariable(method = "tickEffects", at = @At("STORE"), ordinal = 0)
+    private List<ParticleOptions> removeFirstPersonEffectParticles(List<ParticleOptions> particles) {
+        LivingEntity self = (LivingEntity)(Object)this;
 
         Minecraft minecraft = Minecraft.getInstance();
-        if (ConfigScreen.CONFIG.instance().hideFirstPersonEffectParticles && entity == minecraft.player &&
-                minecraft.options.getCameraType().isFirstPerson()) {
-            ci.cancel();
+        if (ConfigScreen.CONFIG.instance().hideFirstPersonEffectParticles && self == minecraft.player && minecraft.options.getCameraType().isFirstPerson()) {
+            return List.of();
         }
+        return particles;
     }
 }
